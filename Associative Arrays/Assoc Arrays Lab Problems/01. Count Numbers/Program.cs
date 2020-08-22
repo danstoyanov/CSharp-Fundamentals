8c9f3c4a0c91be2e5cf11c1e86f666ad24741709
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-22 14:04:53 +0300

    update

---
+++ Associative Arrays/Assoc Arrays Lab Problems/01. Count Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_Count_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList();
+
+            var dict = new SortedDictionary<double, int>();
+
+            foreach (var num in numbers)
+            {
+                if (dict.ContainsKey(num))
+                {
+                    dict[num]++;
+                }
+                else
+                {
+                    dict.Add(num, 1);
+                }
+            }
+
+            foreach (var num in dict)
+            {
+                Console.WriteLine($"{num.Key} -> {num.Value}");
+            }
+        }
+    }
+}
