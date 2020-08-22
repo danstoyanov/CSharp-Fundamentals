f0f3d0dcde62c602decf3ead2eba344f30689268
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-22 13:39:40 +0300

    update

---
+++ Associative Arrays/Assoc Arrays Lab Problems/02_Odd_Occurrences/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine()
+                .ToLower()
+                .Split()
+                .ToList();
+
+            var dictWords = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (!dictWords.ContainsKey(word))
+                {
+                    dictWords.Add(word, 1);
+                }
+                else
+                {
+                    dictWords[word]++;
+                }
+            }
+
+            foreach (var word in dictWords)
+            {
+                if (word.Value % 2 != 0)
+                {
+                    Console.Write(word.Key + " ");
+                }
+            }
+        }
+    }
+}
