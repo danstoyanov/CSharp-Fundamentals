b6af738d1972434cdb32e5158df457bd0c642424
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-23 10:46:41 +0300

    update

---
+++ Associative Arrays/Assoc Arrays Lab Problems/05. Word Filter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace _05_Word_Filter
+{
+    class Program
+    {
+        static void Main()
+        {
+            var list = Console.ReadLine()
+                .Split()
+                .Where(w => w.Length % 2 == 0)
+                .ToList();
+
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
