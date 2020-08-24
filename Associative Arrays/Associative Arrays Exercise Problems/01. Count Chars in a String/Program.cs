30cd973785889e520bf1c66d72dd0348a5b1cc2e
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-24 13:26:26 +0300

    update

---
+++ Associative Arrays/Associative Arrays Exercise Problems/01. Count Chars in a String/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+
+            var dict = new Dictionary<char, int>();
+
+            foreach (var currElement in text)
+            {
+                if (currElement != ' ')
+                {
+                    if (!dict.ContainsKey(currElement))
+                    {
+                        dict.Add(currElement, 1);
+                    }
+                    else
+                    {
+                        dict[currElement]++;
+                    }
+                }
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item.Key + " -> " + item.Value);
+            }
+        }
+    }
+}
---
+++ Associative Arrays/Associative Arrays Exercise Problems/02. A Miner Task/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02_A_Miner_Task
+{
+    class Program
+    {
+        static void Main()
+        {
+            string resourse = string.Empty;
+
+            var inventory = new Dictionary<string, int>();
+
+            while ((resourse = Console.ReadLine()) != "stop")
+            {
+                int value = int.Parse(Console.ReadLine());
+
+                if (!inventory.ContainsKey(resourse))
+                {
+                    inventory.Add(resourse, value);
+                }
+                else
+                {
+                    inventory[resourse] += value;
+                }
+            }
+
+            foreach (var pair in inventory)
+            {
+                Console.WriteLine(pair.Key + " -> " + pair.Value);
+            }
+        }
+    }
+}
