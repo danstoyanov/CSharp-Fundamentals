dca2f13a290226b9f76bbea4492eecccd72b0501
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-24 13:53:39 +0300

    update

---
+++ Text Processing/Text Proc Lab Problems/02. Repeat Strings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _02_Repeat_Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            var words = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            var result = new StringBuilder();
+
+            foreach (var word in words)
+            {
+                int count = word.Length;
+
+                for (int i = 0; i < count; i++)
+                {
+                    result.Append(word);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
