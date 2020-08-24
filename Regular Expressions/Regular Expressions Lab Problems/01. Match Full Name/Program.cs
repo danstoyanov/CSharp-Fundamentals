48c140649f236cb4ed64347c990c61324a7adef4
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-24 13:42:55 +0300

    update

---
+++ Regular Expressions/Regular Expressions Lab Problems/01. Match Full Name/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01_Match_Full_Name
+{
+    class Program
+    {
+        static void Main()
+        {
+            string names = Console.ReadLine();
+
+            var regex = new Regex(@"\b[A-Z][a-z]+ \b[A-Z][a-z]+");
+
+            MatchCollection currNames = regex.Matches(names);
+
+            foreach (Match name in currNames)
+            {
+                Console.WriteLine($"{name.Value} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
