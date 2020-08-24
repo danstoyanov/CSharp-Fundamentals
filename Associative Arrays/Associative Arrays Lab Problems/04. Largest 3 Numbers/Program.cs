b3587409c99b9eb23b8b6a88f1267d092161dceb
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-24 13:24:22 +0300

    update

---
+++ Associative Arrays/Associative Arrays Lab Problems/04. Largest 3 Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var integers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            integers.Sort();
+            integers.Reverse();
+
+            if (integers.Count >= 3)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(integers[i] + " ");
+                }
+
+                return;
+            }
+            else
+            {
+                Console.Write(string.Join (" ", integers));
+            }
+        }
+    }
+}
