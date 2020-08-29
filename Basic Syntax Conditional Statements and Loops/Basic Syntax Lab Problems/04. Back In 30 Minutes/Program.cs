75c8fbe2b53f049134780e3379f873b923b7a35d
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-29 16:05:14 +0300

    update

---
+++ Basic Syntax Conditional Statements and Loops/Basic Syntax Lab Problems/04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _04_Back_In_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            // The problem has been solved in Judge with 100/100, but you must 
+            // try to repeat this exercise again !!
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            // 1 - Make hours to minutes !
+            // 2 - calculate totalTime !
+
+            int hoursPerMinuter = hours * 60;
+            int totalTime = (hoursPerMinuter + minutes) + 30;
+
+            // at the End - you must calculate the "hours", like --->> 21 or 09 ...
+            // at the End - you must calculate the "minutes", like --->> 59 or 00 ...
+
+            double hoursToPrint = (totalTime / 60); // hours
+            double minutesToPrint = totalTime * 1.0 % 60; // minutes
+
+            if (hoursToPrint > 23)
+            {
+                if (minutesToPrint <= 9)
+                {
+                    Console.WriteLine($"{0}:0{minutesToPrint}");
+                }
+                else
+                {
+                    Console.WriteLine($"{0}:{minutesToPrint}");
+                }
+            }
+            else
+            {
+                if (minutesToPrint <= 9)
+                {
+                    Console.WriteLine($"{hoursToPrint}:0{minutesToPrint}");
+                }
+                else
+                {
+                    Console.WriteLine($"{hoursToPrint}:{minutesToPrint}");
+                }
+            }
+        }
+    }
+}
+
