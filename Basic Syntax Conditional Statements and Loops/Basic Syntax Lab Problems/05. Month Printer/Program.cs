77f86a4f3ee403136b76a3d9dc30f520e105e7e5
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-29 16:05:42 +0300

    update

---
+++ Basic Syntax Conditional Statements and Loops/Basic Syntax Lab Problems/05. Month Printer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _05_Month_Printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int monthInput = int.Parse(Console.ReadLine());
+            string monthABC = "";
+
+            switch (monthInput)
+            {
+                case 1:
+                    monthABC = "January";
+                    break;
+                case 2:
+                    monthABC = "February";
+                    break;
+                case 3:
+                    monthABC = "March";
+                    break;
+                case 4:
+                    monthABC = "April";
+                    break;
+                case 5:
+                    monthABC = "May";
+                    break;
+                case 6:
+                    monthABC = "June";
+                    break;
+                case 7:
+                    monthABC = "July";
+                    break;
+                case 8:
+                    monthABC = "August";
+                    break;
+                case 9:
+                    monthABC = "September";
+                    break;
+                case 10:
+                    monthABC = "October";
+                    break;
+                case 11:
+                    monthABC = "November";
+                    break;
+                case 12:
+                    monthABC = "December";
+                    break;
+                default:
+                    monthABC = "Error!";
+                    break;
+            }
+
+            Console.WriteLine(monthABC);
+        }
+    }
+}
