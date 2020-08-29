7415f9276530dacc8910024fe702d34cb4cf6057
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-29 16:06:35 +0300

    update

---
+++ Basic Syntax Conditional Statements and Loops/Basic Syntax Lab Problems/06. Foreign Languages/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06_Foreign_Languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string countryNameInput = Console.ReadLine();
+            string languageType = "";
+
+            if (countryNameInput == "USA" || countryNameInput == "England")
+            {
+                languageType = "English";
+            }
+            else if (countryNameInput == "Spain" || countryNameInput == "Argentina" || countryNameInput == "Mexico")
+            {
+                languageType = "Spanish";
+            }
+            else
+            {
+                languageType = "unknown";
+            }
+
+            Console.WriteLine(languageType);
+        }
+    }
+}
