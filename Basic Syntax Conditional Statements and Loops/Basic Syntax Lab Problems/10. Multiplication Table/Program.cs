25e511d2047888250d745f1ee4dd44efed182cb7
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-30 13:01:30 +0300

    update

---
+++ Basic Syntax Conditional Statements and Loops/Basic Syntax Lab Problems/10. Multiplication Table/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _10_Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numInput = int.Parse(Console.ReadLine());
+            int result = 0;
+
+            for (int repeatNums = 1; repeatNums <= 10; repeatNums++)
+            {
+                result = numInput * repeatNums;
+                Console.WriteLine($"{numInput} X {repeatNums} = {result}");
+            }
+        }
+    }
+}
