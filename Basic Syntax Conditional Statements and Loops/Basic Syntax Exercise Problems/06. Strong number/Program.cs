89034f8dbe003d4b126f6a277753a34d40b79d51
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-01 10:38:12 +0300

    update

---
+++ Basic Syntax Conditional Statements and Loops/Basic Syntax Exercise Problems/06. Strong number/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _06_Strong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int tempNum = num;
+            string stringNum = "";
+            stringNum += num;
+
+            int sum = 0;
+
+            for (int i = 0; i < stringNum.Length; i++)
+            {
+                int currentDigit = tempNum % 10;
+                tempNum = (tempNum - currentDigit) / 10; 
+
+                int factorial = 1;
+
+                for (int j = 1; j <= currentDigit; j++)
+                {
+                    factorial *= j;
+                }
+                sum += factorial;
+            }
+
+            if (num == sum)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
