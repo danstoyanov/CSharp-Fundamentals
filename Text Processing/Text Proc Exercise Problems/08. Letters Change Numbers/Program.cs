32796044ebf1710b0606bc2b6200bcf947cf8c36
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-09 21:16:12 +0300

    update

---
+++ Text Processing/Text Proc Exercise Problems/08. Letters Change Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _08_Letters_Change_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
