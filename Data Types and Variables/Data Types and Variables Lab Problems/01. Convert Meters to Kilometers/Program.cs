731e5ec9a5dcbbd422ecb69c0229b73641617926
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-11 17:09:13 +0300

    update

---
+++ Data Types and Variables/Data Types and Variables Lab Problems/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01_Convert_Meters_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // As we know 1000 meters ==> 1 kilometer.... 
+            decimal meterInput = decimal.Parse(Console.ReadLine());
+            decimal converter = meterInput / 1000;
+            // To make correct convert with these inputs, try to use the new variable "decimal" !
+            Console.WriteLine($"{converter:F2}");
+        }
+    }
+}
