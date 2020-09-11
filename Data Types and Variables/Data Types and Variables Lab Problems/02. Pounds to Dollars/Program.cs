53d54a3313b68a44997dbf151e11f54db307be84
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-11 17:09:36 +0300

    update

---
+++ Data Types and Variables/Data Types and Variables Lab Problems/02. Pounds to Dollars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02_Pounds_to_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal inputGBP = decimal.Parse(Console.ReadLine());
+            decimal convertValue = inputGBP * 1.31m;
+            // WARNING !!! --> When you use the decimal funcion, add the symbol after the numbers
+            // 1 km. ---> 1000.0m    //    2.99m BGN, 3.99m USD ....
+            Console.WriteLine($"{convertValue:F3}");
+        }
+    }
+}
