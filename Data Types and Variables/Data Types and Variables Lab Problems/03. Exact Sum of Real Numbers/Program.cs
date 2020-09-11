b63cc594f3f5c0e48b91a8ca6db4082c841187fa
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-11 17:10:28 +0300

    update

---
+++ Data Types and Variables/Data Types and Variables Lab Problems/03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace _03_Exact_Sum_of_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersInput = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 1; i <= numbersInput; i++)
+            {
+                decimal inputNum = decimal.Parse(Console.ReadLine());
+                sum += inputNum;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
