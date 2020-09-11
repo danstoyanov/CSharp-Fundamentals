1738414a1573fbf6dd9e7ff426f14c2cf656b1b4
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-11 17:11:04 +0300

    update

---
+++ Data Types and Variables/Data Types and Variables Lab Problems/04. Centuries to Minutes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04_Centuries_to_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // To complete the problem you must to know: 
+            // 1 centuries = 100 years = 36524 days = 876576 hours = 52594560 minutes !!!
+            int centuryInpit = int.Parse(Console.ReadLine());
+
+            int yearsCalculate = centuryInpit * 100;
+            // to calculate integer with double -- input (int) and then (yearsCalculater * "double/decimal number") !
+            int daysCalculate = (int)(yearsCalculate * 365.2422); 
+            int hoursCalculate = daysCalculate * 24;
+            int minutesCalculate = hoursCalculate * 60;
+
+            Console.WriteLine($"{centuryInpit} centuries = {yearsCalculate} years = {daysCalculate} days = " +
+                $"{hoursCalculate} hours = {minutesCalculate} minutes");
+        }
+    }
+}
