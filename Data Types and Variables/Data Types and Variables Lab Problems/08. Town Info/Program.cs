3b76c61278551780e305de8e347c37ff6029759a
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-12 16:03:57 +0300

    update

---
+++ Data Types and Variables/Data Types and Variables Lab Problems/08. Town Info/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _08_Town_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string townInput = Console.ReadLine();
+            int populationInput = int.Parse(Console.ReadLine());
+            decimal areaInput = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Town {townInput} has population of {populationInput} and area {areaInput} square km.");
+        }
+    }
+}
