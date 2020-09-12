c300a63f534df68feec620fdb1056417079e7999
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-12 16:02:39 +0300

    update

---
+++ Data Types and Variables/Data Types and Variables Lab Problems/05. Special Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _05_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numsInput = int.Parse(Console.ReadLine());
+            bool isSpecial = false;
+
+            for (int i = 1; i <= numsInput; i++)
+            {
+                int currentNumber = i;
+                int sum = 0;
+
+                while (currentNumber != 0)
+                {
+                    sum += (currentNumber % 10);
+                    currentNumber /= 10;
+                }
+
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    isSpecial = true;
+                }
+                else
+                {
+                    isSpecial = false;
+                }
+
+                Console.WriteLine($"{i} -> {isSpecial}");
+            }
+        }
+    }
+}
