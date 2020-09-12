a4428a08f39fc6c50f31ecf81469b9cd5288d07a
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-12 16:03:08 +0300

    update

---
+++ Data Types and Variables/Data Types and Variables Lab Problems/06. Reversed Chars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _06_Reversed_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstInput = char.Parse(Console.ReadLine());
+            char middleInput = char.Parse(Console.ReadLine());
+            char lastInput = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{lastInput} {middleInput} {firstInput}");
+        }
+    }
+}
