1c11ee01f361d8239b85ea05ab8fc0946c86be05
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-12 16:03:30 +0300

    update

---
+++ Data Types and Variables/Data Types and Variables Lab Problems/07. Concat Names/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _07_Concat_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string symbol = Console.ReadLine();
+
+            Console.WriteLine($"{firstName}{symbol}{lastName}");
+        }
+    }
+}
