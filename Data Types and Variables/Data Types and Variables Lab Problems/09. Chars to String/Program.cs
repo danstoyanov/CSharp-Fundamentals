710e1ef4cf61bbd56b08ea651d1724a5b7806cce
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-15 19:38:24 +0300

    update

---
+++ Data Types and Variables/Data Types and Variables Lab Problems/09. Chars to String/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _09_Chars_to_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstSymbol = char.Parse(Console.ReadLine());
+            char secondSymbol = char.Parse(Console.ReadLine());
+            char thirdSymbol = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstSymbol}{secondSymbol}{thirdSymbol}");
+        }
+    }
+}
