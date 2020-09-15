d403a0b4a4b49ddbf0022fde8ef4a95ad9c324f3
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-15 19:38:53 +0300

    update

---
+++ Data Types and Variables/Data Types and Variables Lab Problems/10. Lower or Upper/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _10_Lower_or_Upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputSymbol = Console.ReadLine();
+
+            if (inputSymbol == "a" || inputSymbol == "b" || inputSymbol == "c" || inputSymbol == "d" ||
+                inputSymbol == "e" || inputSymbol == "f" || inputSymbol == "g" || inputSymbol == "h" ||
+                inputSymbol == "i" || inputSymbol == "j" || inputSymbol == "k" || inputSymbol == "l" ||
+                inputSymbol == "m" || inputSymbol == "n" || inputSymbol == "o" || inputSymbol == "p" ||
+                inputSymbol == "q" || inputSymbol == "r" || inputSymbol == "s" || inputSymbol == "t" ||
+                inputSymbol == "u" || inputSymbol == "v" || inputSymbol == "w" || inputSymbol == "x" ||
+                inputSymbol == "y" || inputSymbol == "z")
+            {
+                Console.WriteLine("lower-case");
+            }
+            else
+            {
+                Console.WriteLine("upper-case");
+            }
+        }
+    }
+}
