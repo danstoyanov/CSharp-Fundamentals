012e0993d5d465b4fbe255d2b9f409ee3e3ed247
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-12-21 23:45:38 +0200

    update

---
+++ Lecture Methods/Lecture Methods Lab Problems/03_Calculations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _03_Calculations
+{
+    class Program
+    {
+        static void Main()
+        {
+            string calculation = Console.ReadLine();
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Calculation(calculation, a, b);
+        }
+
+        static void Calculation(string calculation, int a, int b)
+        {
+            if (calculation == "add")
+            {
+                Console.WriteLine(a + b);
+            }
+            else if (calculation == "multiply")
+            {
+                Console.WriteLine(a * b);
+            }
+            else if (calculation == "subtract")
+            {
+                Console.WriteLine(a - b);
+            }
+            else if (calculation == "divide")
+            {
+                Console.WriteLine(a / b);
+            }
+        }
+    }
+}
