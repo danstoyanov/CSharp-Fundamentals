2ce9a04ac206fbbd14e0082c82f5069dc13f0bb1
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-12-21 23:45:10 +0200

    update

---
+++ Lecture Methods/Lecture Methods Lab Problems/02_Grades/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02_Grades
+{
+    class Program
+    {
+        static void Main()
+        {
+            double gradeInput = double.Parse(Console.ReadLine());
+            CheckedGrade(gradeInput);
+        }
+        static void CheckedGrade(double gradeInput)
+        {
+            if (gradeInput >= 2.00 && gradeInput <= 2.99)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (gradeInput >= 3.00 && gradeInput <= 3.49)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if (gradeInput >= 3.50 && gradeInput <= 4.49)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (gradeInput >= 4.50 && gradeInput <= 5.49)
+            {
+                Console.WriteLine("Very good");
+            }
+            else if (gradeInput >= 5.50 && gradeInput <= 6.00)
+            {
+                Console.WriteLine("Excellent");
+            }
+        }
+    }
+}
