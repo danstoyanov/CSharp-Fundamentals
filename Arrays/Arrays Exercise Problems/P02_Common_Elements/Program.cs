b182acd35a504d0e92d20c9cfa2671f6d3e11818
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-01-05 15:58:24 +0200

    update

---
+++ Arrays/Arrays Exercise Problems/P02_Common_Elements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P02_Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arratText01 = Console.ReadLine().Split();
+            string[] arratText02 = Console.ReadLine().Split();
+
+            string collectWords = string.Empty;
+
+            for (int i = 0; i < arratText02.Length; i++)
+            {
+                for (int j = 0; j < arratText01.Length; j++)
+                {
+                    if (arratText02[i] == arratText01[j])
+                    {
+                        collectWords += arratText02[i] + " ";
+                    }
+                }
+            }
+
+            Console.WriteLine(collectWords);
+        }
+    }
+}
