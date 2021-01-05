d73524963052154e2b9edfd7ca5e266604a739cd
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-01-05 15:58:03 +0200

    update

---
+++ Arrays/Arrays Exercise Problems/P01_Train/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01_Train
+{
+    class Program
+    {
+        static void Main()
+        {
+            int counter = int.Parse(Console.ReadLine());
+            List<int> nums = new List<int>();
+            int sum = 0;
+
+            for (int i = 0; i < counter; i++)
+            {
+                int numberInput = int.Parse(Console.ReadLine());
+                nums.Add(numberInput);
+
+                sum += numberInput;
+            }
+
+            Console.WriteLine(string.Join(" ", nums));
+            Console.WriteLine(sum);
+        }
+    }
+}
