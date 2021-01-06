44561c52d6b795a0be3cf581994a8acd1f662781
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-01-06 13:56:54 +0200

    update

---
+++ Arrays/Arrays Exercise Problems/P04_Array_Rotation/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace P04_Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int count = int.Parse(Console.ReadLine());
+
+            int curr = 0;
+
+            for (int counter = 0; counter < count; counter++)
+            {
+                curr = array[0];
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    array[i] = array[i + 1];
+                }
+                array[array.Length - 1] = curr;
+            }
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
