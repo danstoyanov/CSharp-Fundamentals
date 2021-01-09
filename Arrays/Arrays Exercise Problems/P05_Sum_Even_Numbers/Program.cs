2b18036ca41356f4dfd6530505e56c8f86542441
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-01-09 14:09:54 +0200

    update

---
+++ Arrays/Arrays Exercise Problems/P05_Sum_Even_Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+
+namespace _05_Sum_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currentValue = nums[i];
+
+                if (currentValue % 2 == 0)
+                {
+                    sum += currentValue;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
