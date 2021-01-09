af14e52e38f24c9996bb2f4b8b83d47fae04ca8f
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-01-09 14:11:39 +0200

    update

---
+++ Arrays/Arrays Exercise Problems/P06_Even_and_Odd_Subtraction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _06_Even_and_Odd_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int evenResult = 0;
+            int oddResult = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currentNumber = nums[i];
+
+                if (currentNumber % 2 == 0)
+                {
+                    evenResult += currentNumber;     
+                }
+                else
+                {
+                    oddResult += currentNumber;                 
+                }
+            }
+
+            Console.WriteLine(evenResult - oddResult);
+
+        }
+    }
+}
