d309c1d3fcd632bca97f1e78819370b44adc421a
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-01-10 20:23:31 +0200

    update

---
+++ Arrays/Arrays Exercise Problems/07_Equal_Arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _07_Equal_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] arr2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] != arr2[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    break;
+                }
+                else
+                {
+                    int currentNum = arr1[i];
+                    sum += currentNum;
+                }
+            }
+
+            bool isEqual = Enumerable.SequenceEqual(arr1, arr2); // This is the new option !!!
+
+            if (isEqual == true)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}
